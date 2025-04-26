8ee481c4cee1e1d57c5ddd01d3f4d467485665a9
MarikPG <120489191+MarikPG@users.noreply.github.com>
2025-04-26 19:37:36 +0300

    +

--- clicker/clicker.py
+++ clicker/clicker.py
@@ -3,8 +3,9 @@ import json
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-player  = {
-    "score":0,
+
+player = {
+    "score": 0,
     "power": 1
 }
 
@@ -14,55 +15,61 @@ def read_data():
         with open("play.json", "r", encoding="utf-8") as file:
             player = json.load(file)
     except:
-        print("file is bor definded")
+        print("невдача((((")
 
 read_data()
+
 
 def save_data():
     global player
     try:
         with open("play.json", "w", encoding="utf-8") as file:
-            json.dump(player,file, indent= 4,ensure_ascii= True)
+            json.dump(player, file, indent=4, ensure_ascii=True)
     except:
-        print("file is bor definded")
+        print("невдача((((")
+
 
 class MainScreen(Screen):
-    def __init__(self, **kw):
+    def __init__(self,**kw):
         super().__init__(**kw)
 
     def on_shop_screen(self, *args):
         self.manager.current = "shop"
 
     def on_enter(self, *args):
-        self.ids.score_lbl.text = "рахунок: " + str(player["score"])
+        self.ids.score_lbl.text = "рахунок: "+ str(player["score"])
 
     def click(self):
-        self.ids.click.size_hint = (1,1)
+        self.ids.click.size_hint = (1, 1)
         read_data()
-        player["score"] += player ["power"]
-        self.ids.score_lbl.text = "рахунок: " + str(player["score"])
+        player["score"] += player["power"]
+        self.ids.score_lbl.text = "рахунок: "+ str(player["score"])
         save_data()
     def unclick(self):
-        self.ids.click.size_hint(0.5, 0.5)
+        self.ids.click.size_hint = (0.5, 0.5)
 
 class SecondScreen(Screen):
-    def __init__(self, **kw):
+    def __init__(self,**kw):
         super().__init__(**kw)
 
 class MenuScreen(Screen):
-    def __init__(self, **kw):
+    def __init__(self,**kw):
         super().__init__(**kw)
-        def on_main_screen(self, *args):
-            self.manager.current  = "main"
-        def on_second_screen(self, *args):
-            self.manager.current  = "second"
+
+    def on_main_screen(self, *args):
+        self.manager.current = "main"
+
+    def on_second_screen(self, *args):
+        self.manager.current = "second"
+
+
 
 class ShopScreen(Screen):
-    def __init__(self, **kw):
+    def __init__(self,**kw):
         super().__init__(**kw)
 
     def on_enter(self, *args):
-        self.ids.money.text  = "рахунок: " + str(player["score"])
+        self.ids.money.text = "Рахунок: " + str(player["score"])
 
     def buy(self, price, power):
         read_data()
@@ -70,19 +77,24 @@ class ShopScreen(Screen):
         if price <= player["score"]:
             player["score"] -= price
             player["power"] += power
-            self.ids.money.text  = "рахунок: " + str(player["score"])
+            self.ids.money.text = "Рахунок: " + str(player["score"])
             save_data()
     def on_main_screen(self, *args):
         self.manager.current = "main"
 
+
 class ClickerApp(App):
-     def build(self):
-         sm = ScreenManager()
-         sm.add_widget(MenuScreen(name = "menu"))
-         sm.add_widget(MainScreen(name = "main"))
-         sm.add_widget(SecondScreen(name = "second"))
-         sm.add_widget(ShopScreen(name = "shop"))
+    def build(self):
+        sm = ScreenManager()
+
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(SecondScreen(name='second'))
+        sm.add_widget(ShopScreen(name='shop'))
+        return sm
+
+
 
 app = ClickerApp()
 app.run()
-         
+
